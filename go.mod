011279e650a8b3748e28d6a2f0a8dda89bebe556
Dan Kegel <dank@kegel.com>
2022-02-26 15:12:54 -0800

    Trivial client for github.com/dankegel/go-api-demo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.17
+
+require github.com/dankegel/go-api-demo v0.0.0-20220226230532-249607b2ff90
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/dankegel/go-api-demo"
+)
+
+func main() {
+	println(apidemo.Answer())
+}
